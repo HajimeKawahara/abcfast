b81bc8df7bf4d8cfde30cb37c11079eb249ac480
Hajime Kawahara <divrot@gmail.com>
2019-02-10 13:41:37 +0900

    started GPU ABC project

---
+++ Readme.md
@@ -0,0 +1,9 @@
+GPU Approximate Baysian Computation
+
+Under development.
+
+## Random Number Generators
+
+Directory: random_gen
+
+- uniform,normal,gamma function, beta function
---
+++ gabc/random_gen/random_normal.py
@@ -0,0 +1,59 @@
+import time
+import matplotlib.pyplot as plt
+import math
+import numpy as np
+import pycuda.autoinit
+import pycuda.driver as cuda
+import pycuda.compiler
+from pycuda.compiler import SourceModule
+
+def gabcrm_module ():
+    source_module = SourceModule("""
+
+    #include <curand_kernel.h>
+
+    extern "C"{
+    __global__ void abcrm(float *x){
+
+    unsigned long seed;
+    unsigned long id;
+    curandState s;
+    
+    seed=10;
+    id = blockIdx.x;
+    curand_init(seed, id, 0, &s);
+
+    x[id] = curand_normal(&s);
+
+    }
+    }
+
+    """,options=['-use_fast_math'],no_extern_c=True)
+
+    return source_module
+
+if __name__ == "__main__":
+    import numpy as np
+    import matplotlib.pyplot as plt
+    
+    print("********************************************")
+    print("Gaussian Random Sampler using curand_kernel.h")
+    print("********************************************")
+
+    nw=1
+    nt=100000
+    nq=1
+    nb = nw*nt*nq 
+    sharedsize=0 #byte
+    x=np.zeros(nb)
+    x=x.astype(np.float32)
+    dev_x = cuda.mem_alloc(x.nbytes)
+    cuda.memcpy_htod(dev_x,x)
+
+    source_module=gabcrm_module()
+    pkernel=source_module.get_function("abcrm")
+    pkernel(dev_x,block=(int(nw),1,1), grid=(int(nt),int(nq)),shared=sharedsize)
+    cuda.memcpy_dtoh(x, dev_x)
+
+    plt.hist(x,bins=100)
+    plt.show()
